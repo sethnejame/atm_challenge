eabbebb096526ceca12091ec4a5897a9ebe3d923
Daria Lykova <daria.lykova@gmail.com>
2019-06-11 12:56:31 +0200

    Started writing the code to test the happy path. Modified the README file

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ User Story:
 - In order to make an withdrawal      
 - The ATM needs to have funds
 
+- As a User               
+- In order to know if my withdrawal was successful or unsuccessful             
+- I want to receive a message with my withdrawal details
+
+- As an ATM operator          
+- In order for the right person to make a withdrawal            
+- I want each person to have an separate account
+
+- As a ATM operator           
+- In order ensure that an Acccount holder can only withdraw funds that he has    balance for           
+- I want to allow a withdrawal only if there are sufficient funds in the account
+
+
 
 TODO
 
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,31 @@
 require './lib/atm.rb'
 
 describe Atm do
-    
+    let(:account) {instance_double('Account')}
+
+    before do
+         #Before each test we need to add an attribute of `balance`
+         # to the `account`object and set the value to `100`
+        allow(account).to receive(:balance).and_return(100)
+        # We also need to allow 'account' to receive the new balance
+        # using the setter method 'balance'
+    end
+
+    it 'allow withdraw if account has enought balance' do
+        # We need to tell the spec what to look for as the responce
+        # and store that in a variable called 'expected_outcome'
+        # Please note that we are ommiting 'bills' part at the moment,
+        # We will modify this test and add that later
+        expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
+        # We need to pass in two arguments to the withdraw method.
+        # The amount of money we want to withdraw and the 'account' object.
+        # The reason we pass in the 'account' object is that the Atm needs
+        # to be able to access information aboutn the 'accounts' balance
+        # in order to be able to clear the transaction.
+        expect(subject.withdraw(45,account)).to eq expected_output
+    end
+   
+
     it 'has to have funds of €1000' do
         expect(subject.funds).to eq 1000
     end
